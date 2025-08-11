1a1acd3dfd6d87ebb63ee2ef854bb74e92aa3ebe
Mercy5-k <kiqsmercy@gmail.com>
2025-08-11 12:38:31 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        # Turn the original word into a sorted list of letters
+        sorted_word = sorted(self.word)
+
+        # Empty list to collect matches
+        matches = []
+
+        # Loop through each word in the list
+        for candidate in word_list:
+            # Make sure we ignore the word if it's the same as the original
+            if candidate.lower() != self.word:
+                # Sort the candidate and compare
+                if sorted(candidate.lower()) == sorted_word:
+                    matches.append(candidate)
+
+        return matches
